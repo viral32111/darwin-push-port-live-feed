ef296842894a5384505a56b7cd8e3864588ec5c2
viral32111 <19510403+viral32111@users.noreply.github.com>
2024-05-05 14:33:04 +0100

    Channels for STOMP frames
    IDE settings & Redis notes too

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use env_file_reader::read_file;
-use std::error::Error;
+use std::{error::Error, process::exit};
+use stomp::{frame::Frame, header::Headers};
+use xml::element::Element;
 
 mod stomp;
 mod xml;
+
+/*
+service:2024:05:03:G79740:location:ABWDXR:departure:staff = 19:54:00
+service:2024:05:03:G79740:location:ABWDXR:departure:public = 19:54:00
+service:2024:05:03:G79740:location:ABWDXR:departure:estimate = 19:54:00
+service:2024:05:03:G79740:location:ABWDXR:platform = 3
+service:2024:05:03:G79740:location:HTRWAPT:arrival:staff = 20:50:30
+service:2024:05:03:G79740:location:HTRWAPT:arrival:working = 20:51:00
+service:2024:05:03:G79740:location:HTRWAPT:arrival:estimate = 20:49:00
+service:2024:05:03:G79740:location:HTRWAPT:departure:staff = 20:52:00
+service:2024:05:03:G79740:location:HTRWAPT:departure:public = 20:52:00
+service:2024:05:03:G79740:location:HTRWAPT:departure:estimate = 20:52:00
+service:2024:05:03:G79740:location:HTRWAPT:platform = 1
+*/
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let environment_variables = read_file(".env")?;
@@ -28,8 +44,136 @@ fn main() -> Result<(), Box<dyn Error>> {
 	connection.subscribe(0, "/topic/darwin.pushport-v16")?;
 	//connection.subscribe(1, "/topic/darwin.status")?;
 
+	for frame in connection.frame_receiver.iter() {
+		match frame {
+			Ok(frame) => {
+				print_frame(frame)?;
+			}
+			Err(error) => {
+				eprintln!("Frame receiver error: {}", error);
+			}
+		}
+	}
+
 	connection.wait()?;
 	connection.close()?;
+
+	Ok(())
+}
+
+/// Displays a STOMP frame in the console.
+fn print_frame(frame: Frame) -> Result<(), Box<dyn std::error::Error>> {
+	if frame.command == "CONNECTED" {
+		return Ok(());
+	}
+
+	if frame.command == "MESSAGE" && frame.body.is_some() {
+		let body = frame.body.unwrap();
+
+		let content_type = frame.headers.iter().find_map(|(name, value)| {
+			if name.eq(Headers::ContentType.as_str()) {
+				return Some(value.to_string());
+			}
+
+			None
+		});
+
+		if content_type.is_some() && content_type.unwrap().eq("application/xml") {
+			let document = xml::parse(&body)?;
+
+			if document.root.name.is_some()
+				&& document.root.name.as_ref().unwrap() == "Pport"
+				&& document.root.attributes.is_some()
+				&& document.root.children.is_some()
+			{
+				let timestamp = document
+					.root
+					.attributes
+					.as_ref()
+					.unwrap()
+					.get("ts")
+					.unwrap();
+				println!("\n@ {}", timestamp);
+
+				let pport = document.root.children.as_ref().unwrap();
+				if pport.len() != 1 {
+					println!("Pport has more than one child?");
+					recursively_print_element_children(&document.root, 0)?;
+
+					exit(0);
+				}
+
+				let ur = pport.first().unwrap();
+				if ur.name.is_some() && ur.name.as_ref().unwrap() == "uR" {
+					let children = ur.children.as_ref().unwrap();
+
+					for child in children.iter() {
+						recursively_print_element_children(child, 0)?;
+					}
+				}
+			} else {
+				println!("XML declaration:");
+				println!(" Version: {}", document.declaration.version);
+				println!(" Encoding: {}", document.declaration.encoding);
+				println!(" Standalone: {}", document.declaration.standalone);
+
+				println!("");
+				recursively_print_element_children(&document.root, 0)?;
+
+				exit(0);
+			}
+		}
+	} else {
+		println!("{}", frame.command);
+
+		for (name, value) in frame.headers.clone() {
+			println!("{}: {}", name, value);
+		}
+
+		println!("");
+
+		if frame.body.is_some() {
+			println!("{}", frame.body.unwrap());
+		}
+
+		exit(0);
+	}
+
+	Ok(())
+}
+
+fn recursively_print_element_children(element: &Element, depth: u32) -> Result<(), Box<dyn Error>> {
+	if element.name.is_some() {
+		println!(
+			"{}<{}>",
+			" ".repeat(depth as usize),
+			element.name.as_ref().unwrap()
+		);
+	}
+
+	if element.value.is_some() {
+		println!(
+			"{}{}",
+			" ".repeat(depth as usize),
+			element.value.as_ref().unwrap()
+		);
+	}
+
+	if element.attributes.is_some() {
+		let attributes = element.attributes.as_ref().unwrap();
+
+		for (name, value) in attributes.map.iter() {
+			println!(" {}{}: {}", " ".repeat(depth as usize), name, value);
+		}
+	}
+
+	if element.children.is_some() {
+		let children = element.children.as_ref().unwrap();
+
+		for child in children.iter() {
+			recursively_print_element_children(child, depth + 1)?;
+		}
+	}
 
 	Ok(())
 }
--- src/stomp/mod.rs
+++ src/stomp/mod.rs
@@ -1,12 +1,8 @@
-use crate::stomp::header::Headers;
-use crate::xml;
-use crate::xml::element::Element;
-
 use self::frame::Frame;
 use std::error::Error;
 use std::io::{Read, Write};
 use std::net::{Shutdown, TcpStream, ToSocketAddrs};
-use std::process::exit;
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread::{spawn, JoinHandle};
 use std::time::Duration;
 
@@ -18,8 +14,9 @@ const DEFAULT_TIMEOUT: Duration = Duration::from_secs(10);
 /// Represents a connection to a STOMP server.
 pub struct Connection {
 	tcp_stream: TcpStream,
-	recieve_thread: Option<JoinHandle<()>>,
+	receive_thread: Option<JoinHandle<()>>,
 	host_header: String,
+	pub frame_receiver: Receiver<Result<Frame, String>>, // String instead of Box<dyn Error> as the latter doesn't implement Send trait
 }
 
 impl Connection {
@@ -60,14 +57,14 @@ impl Connection {
 	/// Waits for the connection to close.
 	pub fn wait(&mut self) -> Result<(), Box<dyn Error>> {
 		// Don't bother if the thread no longer exists
-		if self.recieve_thread.is_none() {
+		if self.receive_thread.is_none() {
 			return Ok(());
 		}
 
 		// Yoink the thread handle & wait for it to finish
-		let result = self.recieve_thread.take().unwrap().join();
+		let result = self.receive_thread.take().unwrap().join();
 		if result.is_err() {
-			return Err("Unable to join recieve thread".into());
+			return Err("Unable to join receive thread".into());
 		}
 
 		Ok(())
@@ -102,161 +99,50 @@ pub fn open(
 	tcp_stream.set_nodelay(true)?;
 	tcp_stream.set_write_timeout(timeout.or(Some(DEFAULT_TIMEOUT)))?;
 
+	let (frame_sender, frame_receiver) = channel();
+
 	// Spawn a thread to listen for incoming bytes
 	let tcp_stream_clone = tcp_stream.try_clone()?;
-	let recieve_thread = spawn(move || {
-		let result = recieve_bytes(tcp_stream_clone); // Blocks until the TCP stream is closed
+	let receive_thread = spawn(move || {
+		let result = receive_bytes(tcp_stream_clone, frame_sender); // Blocks until the TCP stream is closed
 
 		if result.is_err() {
 			let reason = result.err().unwrap_or("Unknown error".into()).to_string();
-			panic!("Unable to recieve bytes: {}", reason);
+			panic!("Unable to receive bytes: {}", reason);
 		}
 	});
 
 	// Give the caller a handle to this connection
 	Ok(Connection {
 		tcp_stream,
-		recieve_thread: Some(recieve_thread),
+		receive_thread: Some(receive_thread),
 		host_header: host.to_string(),
+		frame_receiver,
 	})
 }
 
 /// Continuously waits for bytes from the STOMP server.
-fn recieve_bytes(mut tcp_stream: TcpStream) -> Result<(), Box<dyn Error>> {
-	let mut recieve_buffer = [0; 4096]; // 4 KiB
+fn receive_bytes(
+	mut tcp_stream: TcpStream,
+	frame_sender: Sender<Result<Frame, String>>,
+) -> Result<(), Box<dyn Error>> {
+	let mut receive_buffer = [0; 4096]; // 4 KiB
 	let mut pending_data: Vec<u8> = Vec::new(); // Infinite
 
 	loop {
 		// Try to receive some bytes
-		let recieved_byte_count = tcp_stream.read(&mut recieve_buffer)?;
-		if recieved_byte_count == 0 {
+		let received_byte_count = tcp_stream.read(&mut receive_buffer)?;
+		if received_byte_count == 0 {
 			return Ok(()); // Give up, there's nothing left to receive
 		}
 
 		// Append the received bytes to the unprocessed data
-		pending_data.extend_from_slice(&recieve_buffer[..recieved_byte_count]);
+		pending_data.extend_from_slice(&receive_buffer[..received_byte_count]);
 
 		// Remove any complete frames from the unprocessed data
 		while let Some((frame, end_position)) = frame::parse(&mut pending_data)? {
 			pending_data.drain(..end_position + 1);
-			print_frame(frame)?;
+			frame_sender.send(Ok(frame))?;
 		}
 	}
-}
-
-/// Displays a STOMP frame in the console.
-fn print_frame(frame: Frame) -> Result<(), Box<dyn std::error::Error>> {
-	if frame.command == "CONNECTED" {
-		return Ok(());
-	}
-
-	if frame.command == "MESSAGE" && frame.body.is_some() {
-		let body = frame.body.unwrap();
-
-		let content_type = frame.headers.iter().find_map(|(name, value)| {
-			if name.eq(Headers::ContentType.as_str()) {
-				return Some(value.to_string());
-			}
-
-			None
-		});
-
-		if content_type.is_some() && content_type.unwrap().eq("application/xml") {
-			let document = xml::parse(&body)?;
-
-			if document.root.name.is_some()
-				&& document.root.name.as_ref().unwrap() == "Pport"
-				&& document.root.attributes.is_some()
-				&& document.root.children.is_some()
-			{
-				let timestamp = document
-					.root
-					.attributes
-					.as_ref()
-					.unwrap()
-					.get("ts")
-					.unwrap();
-				println!("\n@ {}", timestamp);
-
-				let pport = document.root.children.as_ref().unwrap();
-				if pport.len() != 1 {
-					println!("Pport has more than one child?");
-					recursively_print_element_children(&document.root, 0)?;
-
-					exit(0);
-				}
-
-				let ur = pport.first().unwrap();
-				if ur.name.is_some() && ur.name.as_ref().unwrap() == "uR" {
-					let children = ur.children.as_ref().unwrap();
-
-					for child in children.iter() {
-						recursively_print_element_children(child, 0)?;
-					}
-				}
-			} else {
-				println!("XML declaration:");
-				println!(" Version: {}", document.declaration.version);
-				println!(" Encoding: {}", document.declaration.encoding);
-				println!(" Standalone: {}", document.declaration.standalone);
-
-				println!("");
-				recursively_print_element_children(&document.root, 0)?;
-
-				exit(0);
-			}
-		}
-	} else {
-		println!("{}", frame.command);
-
-		for (name, value) in frame.headers.clone() {
-			println!("{}: {}", name, value);
-		}
-
-		println!("");
-
-		if frame.body.is_some() {
-			println!("{}", frame.body.unwrap());
-		}
-
-		exit(0);
-	}
-
-	Ok(())
-}
-
-fn recursively_print_element_children(element: &Element, depth: u32) -> Result<(), Box<dyn Error>> {
-	if element.name.is_some() {
-		println!(
-			"{}<{}>",
-			" ".repeat(depth as usize),
-			element.name.as_ref().unwrap()
-		);
-	}
-
-	if element.value.is_some() {
-		println!(
-			"{}{}",
-			" ".repeat(depth as usize),
-			element.value.as_ref().unwrap()
-		);
-	}
-
-	if element.attributes.is_some() {
-		let attributes = element.attributes.as_ref().unwrap();
-
-		for (name, value) in attributes.map.iter() {
-			println!(" {}{}: {}", " ".repeat(depth as usize), name, value);
-		}
-	}
-
-	if element.children.is_some() {
-		let children = element.children.as_ref().unwrap();
-
-		for child in children.iter() {
-			recursively_print_element_children(child, depth + 1)?;
-		}
-	}
-
-	Ok(())
 }
